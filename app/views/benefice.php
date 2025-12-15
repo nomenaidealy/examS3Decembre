c6c9b42de1fe5cbb7efd0dc5ced0305c856dde1b
nomenaidealy <idealyanjara2@gmail.com>
2025-12-15 14:40:05 +0300

    benefice

--- app/views/benefice.php
+++ app/views/benefice.php
@@ -1,30 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Bénéfice Véhicule</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<style>
+
+</style>
 </head>
 <body>
-    <h1>Total montant bénéfice par véhicule</h1>
-    <table border = "1">
-        <tr>
-            <th>vehicule</th>
-            <th>Date</th>
-            <th>benefice_total</th>
-        </tr>
-        <tr>
-            <?php foreach($data as $row): ?>
-                <tr>
-                    <td><?php echo htmlspecialchars($row['vehicule']); ?></td>
-                    <td><?php echo htmlspecialchars($row['date']); ?></td>
-                    <td><?php echo htmlspecialchars($row['benefice_total']); ?></td>
-                </tr>
-            <?php endforeach; ?>
-        </tr>
+<h1>Total montant bénéfice par véhicule</h1>
 
-    </table>
+<div class="container-main">
+  <table>
+    <thead>
+      <tr>
+        <th>vehicule</th>
+        <th>Date</th>
+        <th>benefice_total</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php foreach($data as $row): ?>
+      <tr>
+        <td><?php echo htmlspecialchars($row['vehicule']); ?></td>
+        <td><?php echo htmlspecialchars($row['date_jour']); ?></td>
+        <td><?php echo htmlspecialchars($row['benefice']); ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+</div>
 </body>
 </html>
